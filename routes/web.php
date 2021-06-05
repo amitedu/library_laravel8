88e52d86e29d76671b62718950937a525d83d935
amit <amitedu21@gmail.com>
2021-06-05 13:31:15 +0530

    Feature: add author resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,6 @@ Route::get('/', function () {
 
 Route::post('/books', [BookController::class, 'store']);
 Route::patch('/books/{book}', [BookController::class, 'update']);
+Route::delete('books/{book}', [BookController::class, 'destroy']);
+
+Route::post('/author', [AuthorController::class, 'store']);
---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_an_author_can_be_created(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+            'name' => 'amit',
+            'dob' => '05/06/2021',
+        ]);
+
+        $authors = Author::all();
+
+        self::assertCount(1, $authors);
+        self::assertInstanceOf(Carbon::class, $authors->first()->dob);
+        self::assertEquals('05/06/2021', $authors->first()->dob->format('m/d/Y'));
+    }
+}
